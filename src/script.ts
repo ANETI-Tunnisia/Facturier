ca63d47c6a8cc9d1dd2275ea00e021cecd3c778b
ANETI-Tunnisia <abdelwaheb_ammar@hotmail.fr>
2024-12-16 00:46:38 +0100

    Exercice Facturier 9

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,8 @@
 import  {FormInput} from "./classes/FormInputs.js";
+import  {Storage}   from "./classes/Storage.js";
 
 new FormInput();
+Storage.checkStorage();
 
 /*form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
